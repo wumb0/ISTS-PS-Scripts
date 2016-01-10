573c3299595346cdeede5cc4ed39dd77b51c75ad
jgeigerm <jmg2967@rit.edu>
2016-01-10 14:11:22 -0500

    initial commit

---
+++ Deploy-ISTSDomain.ps1
@@ -0,0 +1,39 @@
+﻿param (
+    [Parameter(Mandatory=$true)][int]$TeamNumber,
+    [switch]$InstallRoles = $false,
+    [switch]$Uninstall = $false,
+    [switch]$DNSForward = $false,
+    [switch]$All = $false
+)
+
+if ($All){
+    $InstallRoles = $true
+    $DNSForward = $true
+}
+
+$SMPass = ConvertTo-SecureString "Password1!" -AsPlainText -Force
+if ($Uninstall){
+    Write-Host "Uninstalling DNS and ADDS Roles"
+    Uninstall-ADDSDomainController -LastDomainControllerInDomain -NoRebootOnCompletion -IgnoreLastDCInDomainMismatch -RemoveDnsDelegation -Confirm:$false -SkipPreChecks -LocalAdministratorPassword $SMPass
+    UnInstall-WindowsFeature -Name DNS
+    UnInstall-WindowsFeature -Name AD-Domain-Services
+    UnInstall-WindowsFeature -Name RSAT-ADDS
+    UnInstall-WindowsFeature -Name RSAT-DNS-Server
+    exit
+}
+
+if ($InstallRoles){
+    Write-Host "Installing DNS and ADDS Roles"
+    Install-WindowsFeature -Name DNS
+    Install-WindowsFeature -Name AD-Domain-Services
+    Install-WindowsFeature -Name RSAT-ADDS
+    Install-WindowsFeature -Name RSAT-DNS-Server
+    Import-Module ADDSDeployment
+    Install-ADDSForest -SkipPreChecks -DomainName "team$TeamNumber.ists" -SafeModeAdministratorPassword $SMPass -DomainMode Win2008R2 -NoRebootOnCompletion -ForestMode Win2003 -DomainNetbiosName "TEAM$TeamNumber" -Force
+}
+
+if ($DNSForward){
+    Write-Host "Setting DNS server to forward queries to to 10.2.$TeamNumber.60"
+    Get-DnsServerForwarder -ErrorAction SilentlyContinue | Remove-DnsServerForwarder -Force -ErrorAction SilentlyContinue
+    Set-DnsServerForwarder -IPAddress 10.2.$TeamNumber.60
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ISTS-PS-Scripts
+Useful scripts to configure VMs with PowerCLI and to deploy AD domains
